3ebb9f167bf218fd374e5d764a477a0257aa0bcc
pkritt <krittapat37819@gmail.com>
2025-08-04 23:50:55 +0700

    #

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 export default function CounterPage() {
+const [count, setCount] = useState(0); 
+const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="container text-center">
       <h2>Counter Page</h2>
       {/* Result Text */}
-      <p>Counter:</p>
-      <button>Increase</button>
+      <p>Counter: {count}</p>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   );
 }
--- src/pages/HelloMyNamePage.tsx
+++ src/pages/HelloMyNamePage.tsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
+
 export default function HelloMyNamePage() {
+
+const [name, setName] = useState("");
+const [greeting, setGreeting] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleGreet = () => {
+    if (name.trim() === "") {
+      setGreeting("Please input your name!");
+    } else {
+      setGreeting(`Hello, ${name}!`);
+    }
+  };
+
   return (
     <div className="container text-center">
       <h3>Hello My Name</h3>
@@ -6,10 +25,12 @@ export default function HelloMyNamePage() {
         className="form-control d-inline m-1"
         placeholder="Insert your name here!"
         style={{ width: "300px" }}
+        value={name}
+        onChange={handleInputChange}
       />
-      <button className="btn btn-primary">Greet Me</button>
+      <button className="btn btn-primary"  onClick={handleGreet} >Greet Me</button>
       {/* Result Text */}
-      <p></p>
+      <p>{greeting}</p>
     </div>
   );
 }
